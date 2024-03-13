666c9ef2b004c570efecafdd747379d85f655da3
zoyron <sgrarora3@gmail.com>
2024-03-14 04:56:39 +0530

    added the ambient and directional light

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ import GUI from "lil-gui";
  */
 // Debug
 const gui = new GUI();
-
+gui.close();
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -17,14 +17,18 @@ const scene = new THREE.Scene();
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+// Ambient light globally illuminates all objects in the scene equally.
+// this light cannot be used to cast shadows as it does not have a direction. it is the omnidirectional lighting
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // 0.5 because had to make it dim light
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 50);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+// directional light will provide light with all light rays coming from the same direction. all rays will be parallel
+const directionalLight = new THREE.DirectionalLight(0x0000ff, 0.9);
+directionalLight.position.set(1, 0.25, 0); // this is making the directional light come from the right-hand side.
+scene.add(directionalLight);
+
+
+
 
 /**
  * Objects
@@ -32,22 +36,23 @@ scene.add(pointLight);
 // Material
 const material = new THREE.MeshStandardMaterial();
 material.roughness = 0.4;
+//material.wireframe = true;
 
 // Objects
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
-sphere.position.x = -1.5;
+sphere.position.y = -1.5;
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
+const cube = new THREE.Mesh(new THREE.BoxGeometry(0.5, 0.5, 0.5), material);
 
 const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+  new THREE.TorusGeometry(0.25, 0.085, 16, 32),
   material,
 );
-torus.position.x = 1.5;
+torus.position.y = 1;
 
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(3, 3), material);
 plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.65;
+plane.position.y = -2;
 
 scene.add(sphere, cube, torus, plane);
 
@@ -83,9 +88,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100,
 );
-camera.position.x = 1;
+camera.position.x = 0;
 camera.position.y = 1;
-camera.position.z = 2;
+camera.position.z = 4;
 scene.add(camera);
 
 // Controls
