865c4af5965d50681f2e5249c170924bf2d0488d
zoyron <sgrarora3@gmail.com>
2024-03-18 13:29:55 +0530

    added the new script.js file

--- src/script.js
+++ src/script.js
@@ -19,18 +19,37 @@ const scene = new THREE.Scene();
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 gui.add(ambientLight, "intensity").min(0).max(3).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(2, 2, -1);
 gui.add(directionalLight, "intensity").min(0).max(3).step(0.001);
 gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
 gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001);
 scene.add(directionalLight);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 6;
+directionalLight.shadow.radius = 5; // this radius won't work when PCFShadowmap is in use
+
+// spotlight
+const spotLight = new THREE.SpotLight(0xffffff, 3.6, 10, Math.PI*0.3);
+spotLight.castShadow = true;
+spotLight.position.set(0,2,2);
+scene.add(spotLight);
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+//const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+spotLight.shadow.camera.near = 1;
+spotLight.shadow.camera.far = 3;
+//scene.add(spotLightCameraHelper);
+
 
 /**
  * Materials
@@ -48,8 +67,9 @@ sphere.castShadow = true;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(2.5, 2.5), material);
 plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.5;
+plane.position.y = -0.25;
 plane.receiveShadow = true;
+material.side = THREE.DoubleSide;
 
 scene.add(sphere, plane);
 
@@ -103,6 +123,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /**
  * Animate
@@ -111,7 +132,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
+  sphere.position.x = Math.cos(elapsedTime) * 0.275;
+  sphere.position.z = Math.sin(elapsedTime) * 0.275;
+  sphere.position.y = Math.abs(Math.cos(elapsedTime) * 0.75);
   // Update controls
   controls.update();
 
