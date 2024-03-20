5c40796393eab85984a841b9d1f7e80d80961e7d
zoyron <sgrarora3@gmail.com>
2024-03-20 23:14:18 +0530

    added the spin

--- src/script.js
+++ src/script.js
@@ -16,34 +16,64 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Galaxy 
+ * Galaxy
  */
 const parameters = {};
-parameters.count = 1000;
+parameters.count = 2000;
+parameters.size = 0.05;
+parameters.radius = 5;
+parameters.branches = 3;
+parameters.spin = 1;
+
+let geometry = null;
+let positions = null;
+let material = null;
+let particles = null;
 
 const generateGalaxy = () => {
-  const geometry = new THREE.BufferGeometry();
-  const positions = new Float32Array(parameters.count * 3);
+  // destroy old galaxy
+  if(particles != null){
+    geometry.dispose();
+    material.dispose();
+    scene.remove(particles);
+  }
+
+
+  geometry = new THREE.BufferGeometry();
+  positions = new Float32Array(parameters.count * 3);
 
   // filling up the co-ordinates of the particles of the geometry
-  for(let i = 0 ; i < parameters.count * 3 ; i++){
-    positions[i] = (Math.random() - 0.5) * 6;
+  for (let i = 0; i < parameters.count ; i++) {
+    const i3 = i*3;
+
+    const radius = Math.random() * parameters.radius;
+    const branchesAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
+
+
+    positions[i3] = Math.cos(branchesAngle) * radius;
+    positions[i3+1] = Math.random();
+    positions[i3+2] = Math.sin(branchesAngle) * radius;
   }
-  geometry.setAttribute(
-    'position',
-    new THREE.BufferAttribute(positions, 3),
-  );
-  
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
   // creating the material for the geometry
-  const material = new THREE.PointsMaterial({
-    size: 0.05,
+  material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
   });
 
   // creating the particles
-  const particles = new THREE.Points(geometry, material);
+  particles = new THREE.Points(geometry, material);
   scene.add(particles);
-}
+};
 generateGalaxy();
+gui.add(parameters, 'count').min(100).max(30000).step(100).onFinishChange(generateGalaxy);
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, 'radius').min(0.1).max(20).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'branches').min(2).max(12).step(1).onFinishChange(generateGalaxy);
+gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
 
 
 /**
