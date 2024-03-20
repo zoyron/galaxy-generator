7caf91379c268b8e9084df0624e29b7c7acf62c7
zoyron <sgrarora3@gmail.com>
2024-03-20 23:59:49 +0530

    added the color attribute using the colors array

--- src/script.js
+++ src/script.js
@@ -19,18 +19,20 @@ const scene = new THREE.Scene();
  * Galaxy
  */
 const parameters = {};
-parameters.count = 4000;
+parameters.count = 30000;
 parameters.size = 0.01;
 parameters.radius = 5;
 parameters.branches = 3;
 parameters.spin = 1;
-parameters.randomNess = 0.2;
+parameters.randomNess = 0.682;
+parameters.insideColor = '#ff6060';
+parameters.outsideColor = '#1b3984';
 
 let geometry = null;
 let positions = null;
 let material = null;
 let particles = null;
-
+let colors = null;
 const generateGalaxy = () => {
   // destroy old galaxy
   if(particles != null){
@@ -42,6 +44,7 @@ const generateGalaxy = () => {
 
   geometry = new THREE.BufferGeometry();
   positions = new Float32Array(parameters.count * 3);
+  colors = new Float32Array(parameters.count * 3);
 
   // filling up the co-ordinates of the particles of the geometry
   for (let i = 0; i < parameters.count ; i++) {
@@ -51,14 +54,20 @@ const generateGalaxy = () => {
     const branchesAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
     const spinAngle = radius * parameters.spin;
     const randomX = (Math.random() - 0.5) * parameters.randomNess; 
-    const randomY = (Math.random() - 0.5) * parameters.randomNess; 
+    const randomY = (Math.random() - 0.5) * parameters.randomNess /2; 
     const randomZ = (Math.random() - 0.5) * parameters.randomNess; 
 
     positions[i3] = Math.cos(branchesAngle + spinAngle) * radius + (randomX / radius);
     positions[i3+1] = randomY / radius;
     positions[i3+2] = Math.sin(branchesAngle + spinAngle) * radius + (randomZ / radius);
+
+    // colors
+    colors[i3] = 1;
+    colors[i3 + 1] = 0;
+    colors[i3 + 2] = 0;
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+  geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
   // creating the material for the geometry
   material = new THREE.PointsMaterial({
@@ -73,12 +82,14 @@ const generateGalaxy = () => {
   scene.add(particles);
 };
 generateGalaxy();
-gui.add(parameters, 'count').min(100).max(30000).step(100).onFinishChange(generateGalaxy);
+gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy);
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, 'radius').min(0.1).max(20).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, 'branches').min(2).max(12).step(1).onFinishChange(generateGalaxy);
 gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, 'randomNess').min(0).max(5).step(0.0001).onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy);
 
 
 /**
@@ -138,6 +149,7 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  particles.rotation.y = elapsedTime * 0.05;
 
   // Update controls
   controls.update();
