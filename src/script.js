8112110ff1c9a3dbaf5187fb426714fa91e26e46
zoyron <sgrarora3@gmail.com>
2024-04-18 11:10:04 +0530

    making galaxy with shaders

--- src/script.js
+++ src/script.js
@@ -7,7 +7,6 @@ import GUI from "lil-gui";
  */
 // Debug
 const gui = new GUI();
-gui.close();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -19,83 +18,131 @@ const scene = new THREE.Scene();
  * Galaxy
  */
 const parameters = {};
-parameters.count = 40000;
-parameters.size = 0.01;
-parameters.radius = 4;
-parameters.branches = 4;
+parameters.count = 200000;
+parameters.size = 0.005;
+parameters.radius = 5;
+parameters.branches = 3;
 parameters.spin = 1;
-parameters.randomNess = 0.682;
-parameters.insideColor = '#ff6060';
-parameters.outsideColor = '#1b3984';
+parameters.randomness = 0.5;
+parameters.randomnessPower = 3;
+parameters.insideColor = "#ff6030";
+parameters.outsideColor = "#1b3984";
 
 let geometry = null;
-let positions = null;
 let material = null;
-let particles = null;
-let colors = null;
+let points = null;
+
 const generateGalaxy = () => {
-  // destroy old galaxy
-  if(particles != null){
+  if (points !== null) {
     geometry.dispose();
     material.dispose();
-    scene.remove(particles);
+    scene.remove(points);
   }
 
-
+  /**
+   * Geometry
+   */
   geometry = new THREE.BufferGeometry();
-  positions = new Float32Array(parameters.count * 3);
-  colors = new Float32Array(parameters.count * 3);
-  const colorInside = new THREE.Color(parameters.insideColor);
-  const colorOutside = new THREE.Color(parameters.outsideColor);
 
+  const positions = new Float32Array(parameters.count * 3);
+  const colors = new Float32Array(parameters.count * 3);
 
-  // filling up the co-ordinates of the particles of the geometry
-  for (let i = 0; i < parameters.count ; i++) {
-    const i3 = i*3;
+  const insideColor = new THREE.Color(parameters.insideColor);
+  const outsideColor = new THREE.Color(parameters.outsideColor);
 
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+
+    // Position
     const radius = Math.random() * parameters.radius;
-    const branchesAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
-    const spinAngle = radius * parameters.spin;
-    const randomX = (Math.random() - 0.5) * parameters.randomNess; 
-    const randomY = (Math.random() - 0.5) * parameters.randomNess /2; 
-    const randomZ = (Math.random() - 0.5) * parameters.randomNess; 
 
-    positions[i3] = Math.cos(branchesAngle + spinAngle) * radius + (randomX / radius);
-    positions[i3+1] = randomY / radius;
-    positions[i3+2] = Math.sin(branchesAngle + spinAngle) * radius + (randomZ / radius);
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-    // colors
-    const mixedColor = colorInside.clone();
-    mixedColor.lerp(colorOutside, radius/parameters.radius);
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+
+    positions[i3] = Math.cos(branchAngle) * radius + randomX;
+    positions[i3 + 1] = randomY;
+    positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ;
+
+    // Color
+    const mixedColor = insideColor.clone();
+    mixedColor.lerp(outsideColor, radius / parameters.radius);
+
     colors[i3] = mixedColor.r;
     colors[i3 + 1] = mixedColor.g;
     colors[i3 + 2] = mixedColor.b;
   }
+
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
   geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
-  // creating the material for the geometry
+  /**
+   * Material
+   */
   material = new THREE.PointsMaterial({
     size: parameters.size,
     sizeAttenuation: true,
     depthWrite: false,
     blending: THREE.AdditiveBlending,
-    vertexColors: true
+    vertexColors: true,
   });
 
-  // creating the particles
-  particles = new THREE.Points(geometry, material);
-  scene.add(particles);
+  /**
+   * Points
+   */
+  points = new THREE.Points(geometry, material);
+  scene.add(points);
 };
-generateGalaxy();
-gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy);
-gui.add(parameters, 'radius').min(0.1).max(20).step(0.01).onFinishChange(generateGalaxy);
-gui.add(parameters, 'branches').min(2).max(12).step(1).onFinishChange(generateGalaxy);
-gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
-gui.add(parameters, 'randomNess').min(0).max(5).step(0.0001).onFinishChange(generateGalaxy);
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy);
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy);
 
+generateGalaxy();
+
+gui
+  .add(parameters, "count")
+  .min(100)
+  .max(1000000)
+  .step(100)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "randomness")
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "randomnessPower")
+  .min(1)
+  .max(10)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui.addColor(parameters, "insideColor").onFinishChange(generateGalaxy);
+gui.addColor(parameters, "outsideColor").onFinishChange(generateGalaxy);
 
 /**
  * Sizes
@@ -130,8 +177,8 @@ const camera = new THREE.PerspectiveCamera(
   100,
 );
 camera.position.x = 3;
-camera.position.y = 4;
-camera.position.z = 4;
+camera.position.y = 3;
+camera.position.z = 3;
 scene.add(camera);
 
 // Controls
@@ -154,7 +201,6 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  particles.rotation.y = elapsedTime * 0.05;
 
   // Update controls
   controls.update();
